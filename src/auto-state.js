ff24f9a6e130960b590ae236a08ab4835db2a6bb
Baker Shamlan <root@xyz.dev>
2021-07-17 00:39:02 +0300

    Added auto state mixins

---
+++ src/auto-state.js
@@ -0,0 +1,77 @@
+import Vue from "vue";
+import {
+    forEach,
+    pick,
+    omit
+} from "lodash";
+
+const defaults = {
+    autoState: {
+        cacheKey: 'state'
+    }
+}
+
+export default function activateAutoState(storage, opts) {
+    let $storage = storage;
+    let $options = { ...defaults, ...opts };
+    Vue.mixin({
+        mixins: [{
+            watch: {
+                $data: {
+                    handler() {
+                        this.saveState();
+                    },
+                    deep: true
+                }
+            },
+
+            created() {
+                this.loadState();
+            },
+
+            beforeMount() {
+                this.loadState();
+            },
+            methods: {
+                loadState() {
+                    const savedState = $storage.getUniversal(
+                        $options.autoState.cacheKey
+                    );
+
+                    if (!savedState) {
+                        return;
+                    }
+
+                    forEach(this._sanitizeData(savedState, {
+                        saveProperties: $options.autoState.saveProperties,
+                        ignoreProperties: $options.autoState.ignoreProperties
+                    }), (value, key) => {
+                        /*  if (this.autoStateConfig().onLoad) {
+                           value = this.autoStateConfig().onLoad(key, value);
+                         } */
+                        this.$data[key] = value;
+                    });
+                },
+                saveState() {
+                    const data = this._sanitizeData(this.$data, {
+                        saveProperties: $options.autoState.saveProperties,
+                        ignoreProperties: $options.autoState.ignoreProperties
+                    });
+
+                    $storage.setUniversal($options.autoState.cacheKey, data);
+
+                },
+                _sanitizeData(data, config) {
+                    let { saveProperties, ignoreProperties } = config || $options.autoState
+                    let result = (ignoreProperties ? omit(data, ignoreProperties) : data)
+                    return (saveProperties ? pick(result, saveProperties) : result);
+                },
+
+                clearSavedState() {
+                    $storage.removeUniversal($options.autoState.cacheKey);
+                }
+            }
+        }]
+    }); // Set up your mixin then
+    /* } */
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 /* import hello from './hello';
 import hello2 from './hello2'; */
+import autoState from './auto-state';
 import sanitize from './sanitize.js';
 //const Library = {}
 //console.log(Library);
 export {
 /*     hello,
     hello2, */
-    sanitize
+    sanitize,
+    autoState
 };
